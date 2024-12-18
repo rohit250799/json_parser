98d84e341b1c2341dca6c2f216fac8346b01f7b3
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-12-18 18:43:44 +0530

    feat: tokenizing provided file contents

--- accept_file.py
+++ accept_file.py
@@ -1,4 +1,5 @@
 import argparse
+import tokenize
 
 parser = argparse.ArgumentParser(prog='file_acceptance', description='accept name of file')
 parser.add_argument('filename', help='add filename for validity checking')
@@ -9,4 +10,9 @@ filename_supplied_as_argument = parser.parse_args()
     
 actual_filename = filename_supplied_as_argument.filename
 print(actual_filename)
+
+with tokenize.open(actual_filename) as f:
+    tokens = tokenize.generate_tokens(f.readline)
+    for token in tokens:
+        print(token)
 
