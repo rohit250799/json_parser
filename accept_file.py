14071964021e9566c5c17e9c1fc773ef99250b83
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-12-19 22:04:54 +0530

    add: 2 functions to find total char length in file and generate tokens from the file

--- accept_file.py
+++ accept_file.py
@@ -11,8 +11,14 @@ filename_supplied_as_argument = parser.parse_args()
 actual_filename = filename_supplied_as_argument.filename
 print(actual_filename)
 
-with tokenize.open(actual_filename) as f:
-    tokens = tokenize.generate_tokens(f.readline)
-    for token in tokens:
-        print(token)
+def total_char_length_in_file(actual_filename) -> int:
+    with open(actual_filename) as f:
+        file_content = f.read()
+        total_char_length = len(file_content)
+    return total_char_length
 
+def generating_tokens_from_file(actual_filename):
+    with tokenize.open(actual_filename) as f:
+        tokens = tokenize.generate_tokens(f.readline)
+        for token in tokens:
+            print(token)
